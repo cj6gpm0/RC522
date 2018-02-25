23c3293df1835314e6d8131df6893947a2d8fa1a
cj6gpm0 <36371065+cj6gpm0@users.noreply.github.com>
2018-02-25 21:30:47 +0800

    1

--- mjs_fs/api_rc522.js
+++ mjs_fs/api_rc522.js
@@ -1,7 +1,18 @@
 let rc522= {
   init : ffi('void rc522_init_set(void)'),
-  near: ffi('int rc522_card_near_c2mjs(int)'),
-  
+  near_c2mjs: ffi('int rc522_card_near_c2mjs(int)'),
+  near: function(){
+    let cdata=[0,0,0,0,0,0,0];
+    this.card_c2mjs(cdata);
+    return cdata;
+  },
+  card_c2mjs :function(card_data){
+    let count =0;
+    while(count>7){
+      card_data[count]=this.near_c2mjs(count);
+      count++;
+    }
+  },
   c2hex: function (value){
     let i = 0;
     while(value >= 16){
